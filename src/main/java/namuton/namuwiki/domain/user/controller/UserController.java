fe7de6492b9208d5b78d8de3653fb7afe4f645cd
hjeongb0320 <hjeongb0320@naver.com>
2023-12-29 07:12:34 +0900

    feat/user_end
    user 부분 관련하여 준 완성본

--- src/main/java/namuton/namuwiki/domain/user/controller/UserController.java
+++ src/main/java/namuton/namuwiki/domain/user/controller/UserController.java
@@ -1,10 +1,14 @@
 package namuton.namuwiki.domain.user.controller;
 
+import namuton.namuwiki.domain.user.domain.User;
 import namuton.namuwiki.domain.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -19,5 +23,25 @@ public class UserController {
     @GetMapping("/total")
     public ResponseEntity<Integer> getTotalDeletedEmail() {
         return userService.getTotalDarkDataCount();
+    }
+
+    @GetMapping("/myScore")
+    public ResponseEntity<Integer> getDeletedEmail(@RequestParam Long userId) {
+        return userService.getDeletedEmail(userId);
+    }
+
+    @GetMapping("/rankBoard")
+    public ResponseEntity<List<User>> getRankBoard() {
+        return userService.getRankBoard();
+    }
+
+    @GetMapping("/myRank")
+    public ResponseEntity<Integer> getUserRank(@RequestParam Long userId) {
+        return userService.getUserRank(userId);
+    }
+
+    @GetMapping("/percent")
+    public ResponseEntity<Double> getUserPercent(@RequestParam Long userId) {
+        return userService.getUserPercent(userId);
     }
 }
--- src/main/java/namuton/namuwiki/domain/user/repository/UserRepository.java
+++ src/main/java/namuton/namuwiki/domain/user/repository/UserRepository.java
@@ -1,10 +1,23 @@
 package namuton.namuwiki.domain.user.repository;
 
 import namuton.namuwiki.domain.data.domain.Data;
+import namuton.namuwiki.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<Data, Long> {
+
+    @Query("SELECT u.darkDataCnt FROM User u WHERE u.id = :userId")
+    Integer findDarkDataCntByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT u FROM User u ORDER BY u.darkDataCnt DESC")
+    List<User> findAllOrderByDarkDataCntDesc();
+
+    @Query("SELECT COUNT(u) + 1 FROM User u WHERE u.darkDataCnt > (SELECT u2.darkDataCnt FROM User u2 WHERE u2.id = :userId)")
+    Integer findRankByUserId(@Param("userId") Long userId);
 
     @Query("SELECT SUM(d.darkDataCnt) FROM User d")
     Integer findTotalDarkDataCnt();
