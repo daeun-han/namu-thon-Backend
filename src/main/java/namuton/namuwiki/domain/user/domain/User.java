48d7c7e1b4efa7b5296a72e6e11dfeb41e24e772
djc06048 <gsafe1213@gmail.com>
2023-12-28 20:29:50 +0900

    feat: initial commit

---
+++ src/main/java/namuton/namuwiki/domain/user/domain/User.java
@@ -0,0 +1,23 @@
+package namuton.namuwiki.domain.user.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String nickname;
+    private String profileImg;
+
+}
---
+++ src/test/java/namuton/namuwiki/NamuwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package namuton.namuwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamuwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
